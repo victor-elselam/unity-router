b6b03e11177bc3fefc8dfcb8b528ec6305eb0836
Victor Elselam <62479476+victor-elselam@users.noreply.github.com>
2021-12-20 22:22:26 -0300

    Add improved parameters creation

---
+++ Assets/Package/Navigation/Scripts/Parameters/IParameterManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Elselam.UnityRouter.Extensions
+{
+    public interface IParameterManager
+    {
+        IParameter Create<T>(string key, T value);
+        IDictionary<string, string> CreateDictionary(params IParameter[] parameters);
+        T GetParamOfType<T>(IDictionary<string, string> parameters, string key);
+    }
+}
---
+++ Assets/Package/Navigation/Scripts/Parameters/ParameterManager.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Elselam.UnityRouter.Extensions
+{
+    public class ParameterManager : IParameterManager
+    {
+        public IParameter Create<T>(string key, T value) => new ParamTyped<T>(key, value);
+
+        public IDictionary<string, string> CreateDictionary(params IParameter[] parameters)
+        {
+            var dictio = new Dictionary<string, string>();
+            foreach (var param in parameters)
+                dictio[param.Key] = param.Value;
+
+            return dictio;
+        }
+
+        public T GetParamOfType<T>(IDictionary<string, string> parameters, string key)
+        {
+            var hasValue = parameters.TryGetValue(key, out string result);
+            if (!hasValue)
+                return default;
+
+            try
+            {
+                var value = JsonConvert.DeserializeObject<T>(result);
+                return value;
+            }
+            catch (Exception e)
+            {
+                var type = typeof(T);
+                var expected = JsonConvert.SerializeObject(Activator.CreateInstance(type));
+                Debug.LogError(
+                    $"Error parsing parameter {key} of type {typeof(T)} \n" +
+                    $"Expected: {expected} \n" +
+                    $"Found: {result} \n" +
+                    $"Exception: {e}");
+                return default;
+            }
+        }
+
+        
+    }
+}
